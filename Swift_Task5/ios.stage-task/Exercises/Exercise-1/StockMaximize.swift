7c2ff3ba1d9fe0360960a84c5d2152febdb246e3
AnastasiaSkoryukova <anastasia_skoryukova@mail.ru>
2021-06-14 18:25:59 +0300

    Task 5

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,78 @@
 import Foundation
 
 class StockMaximize {
-
+    
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
-    }
+        var maxProfit = 0
+        var currentProfit = 0
+        
+        
+        
+        var start = 0
+        var startPlusOne = start + 1
+        let numberOfElements = prices.count
+        guard let firstElement = prices.first else { return 0 }
+        guard let lastElement = prices.last else { return 0 }
+        
+        if numberOfElements == 0 || numberOfElements == 1 {
+            return 0
+        } else if numberOfElements == 2 {
+            if lastElement > firstElement {
+                maxProfit = lastElement - firstElement
+                
+            }
+        }
+        
+        
+        for i in start..<numberOfElements - 1 {
+            for j in startPlusOne...numberOfElements - 1{
+            currentProfit = lastElement - prices[i]
+            maxProfit = max(maxProfit, currentProfit)
+            currentProfit = lastElement - prices[j]
+            maxProfit = max(maxProfit, currentProfit)
+            currentProfit = prices[j] - prices[i]
+            maxProfit = max(maxProfit, currentProfit)
+                
+                if j + 1 <= numberOfElements - 1 {
+            currentProfit = lastElement - prices[i] + lastElement - prices[j] + lastElement - prices[j+1]
+                }
+                
+            maxProfit = max(maxProfit, currentProfit)
+                
+                if j + 2 <= numberOfElements - 1 && i + 2 <= numberOfElements - 1 {
+            currentProfit = (prices[j] - prices[i]) + (prices[j+2] - prices[i+2])
+                }
+                maxProfit = max(maxProfit, currentProfit)
+                
+                
+        }
+        
+        }
+            
+//                for i in start..<numberOfElements - 2 {
+//
+//                    for j in startPlusOne...numberOfElements - 1{
+//                        if prices[j] > prices[i] {
+//
+//
+//                            let newArraySlice = prices[start...i]
+//                            let newArray1 = Array(newArraySlice)
+//                            var newArray2 = [Int]()
+//
+//                            if j < numberOfElements - 4 {
+//                            let newArraySlice2 = prices[j + 2...numberOfElements - 1]
+//                            newArray2 = Array(newArraySlice2)
+//                            }
+//                            currentProfit = prices[j] - prices[i] + countProfit(prices: newArray1) + countProfit(prices: newArray2)
+//                            maxProfit = max(maxProfit, currentProfit)
+//
+//                        }
+//                    }
+//
+//            }
+        
+        return maxProfit
+}
+
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,33 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
+        
+        if message.count < 1 && message.count > 60 {
+            return ""
+        }
+        
+        let characterSet = CharacterSet.init(charactersIn: "[]").union(.alphanumerics).union(.lowercaseLetters)
+        
+        
+        let numberInMessage = message.components(separatedBy: CharacterSet.decimalDigits.inverted)
+        let list = numberInMessage.filter({ $0 != "" })
+
+        print(list)
+        var listOfIntsToCheck = [Int]()
+
+        for element in list {
+            listOfIntsToCheck.append(Int(element) ?? 0)
+        }
+        
+        if !listOfIntsToCheck.allSatisfy({ $0 < 300 }) {
+            return ""
+        }
+
+        
+        if characterSet.isSuperset(of: CharacterSet(charactersIn: message)) {
+            
+        }
+        
         return ""
     }
 }
